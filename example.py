110f5800a26c571d47475e0766525bfd8e809047
Abinanthan47 <Abinanthan247@gmail.com>
2023-11-27 13:34:08 +0000

    Add example.py file to the repository

--- example.py
+++ example.py
@@ -1,4 +1,5 @@
-def git_opeation():
- print("I am adding example.py file to the remote repository.")
-git_opeation()o
+def git_operation():
+    print("I am adding example.py file to the remote repository.")
+
+git_operation()
 
